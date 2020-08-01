acb05f97d44340df83c6883b1de2541384cfd13c
d559alice <alice31122@gmail.com>
2020-08-01 16:06:28 +0800

    backend上傳

---
+++ db.js
@@ -0,0 +1,103 @@
+import dotenv from 'dotenv'
+import mongoose from 'mongoose'
+import beautifulUnique from 'mongoose-beautiful-unique-validation'
+import validator from 'validator'
+
+dotenv.config()
+
+const Schema = mongoose.Schema
+mongoose.connect(process.env.DBURL, { useUnifiedTopology: true })
+mongoose.plugin(beautifulUnique)
+
+const userSchema = new Schema({
+  account: {
+    type: String,
+    minlength: [4, '帳號必須四個字以上'],
+    maxlength: [20, '帳號必須二十個字以下'],
+    unique: '帳號已使用',
+    required: [true, '請輸入帳號']
+  },
+  password: {
+    type: String,
+    required: [true, '請輸入密碼']
+  },
+  name: {
+    type: String
+  },
+  email: {
+    type: String,
+    validate: {
+      // 驗證 function
+      validator (value) {
+        return validator.isEmail(value)
+      },
+      // 錯誤訊息
+      message: '信箱格式錯誤'
+    }
+  },
+  phone: {
+    type: Number
+  },
+  address: {
+    type: String
+  },
+  birth: {
+    type: String
+  },
+  cart: {
+    type: Array
+  },
+  wishlist: {
+    type: Array
+  }
+}, {
+  versionKey: false
+})
+
+const sortSchema = new Schema({
+  sortTitle: {
+    type: String
+  },
+  sortName: {
+    type: String
+  },
+  sortsrc: {
+    type: String
+  }
+}, {
+  versionKey: false
+}
+)
+
+const productSchema = new Schema({
+  sortName: {
+    type: String
+  },
+  productName: {
+    type: String
+  },
+  productsrc: {
+    type: String
+  },
+  productPrice: {
+    type: Number
+  },
+  productDescription: {
+    type: String
+  },
+  productStock: {
+    type: Number
+  }
+})
+
+const users = mongoose.model(process.env.COLLECTION_USER, userSchema)
+const sorts = mongoose.model(process.env.COLLECTION_SORT, sortSchema)
+const products = mongoose.model(process.env.COLLECTION_PRODUCT, productSchema)
+const connection = mongoose.connection
+
+export default {
+  users,
+  sorts,
+  products,
+  connection
+}
---
+++ index.js
@@ -0,0 +1,626 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import cors from 'cors'
+import connectMongo from 'connect-mongo'
+import session from 'express-session'
+import multer from 'multer'
+import md5 from 'md5'
+import dotenv from 'dotenv'
+import path from 'path'
+import FTPStorage from 'multer-ftp'
+import fs from 'fs'
+
+import db from './db.js'
+
+dotenv.config()
+
+const MongoStore = connectMongo(session)
+
+const app = express()
+
+app.use(bodyParser.json())
+app.use(cors({
+  origin (origin, callback) {
+    // 直接開網頁，不是 ajax 時，origin 是 undefined
+    if (origin === undefined) {
+      callback(null, true)
+    } else {
+      if (process.env.ALLOW_CORS === 'true') {
+        // 開發環境，允許
+        callback(null, true)
+      } else if (origin.includes('github')) {
+        // 非開發環境，但是從 github 過來，允許
+        callback(null, true)
+      } else {
+        // 不是開發也不是從 github 過來，拒絕
+        callback(new Error('Not allowed'), false)
+      }
+    }
+  },
+  credentials: true
+}))
+app.use(session({
+  secret: 'zuzu',
+  // 將 session 存入 mongodb
+  store: new MongoStore({
+    // 使用 mongoose 的資料庫連接
+    mongooseConnection: db.connection,
+    // 設定存入的 collection
+    collection: process.env.COLLECTION_SESSION
+  }),
+  // session 有效期間
+  cookie: {
+    // 1000 毫秒 = 一秒鐘
+    // 1000 毫秒 * 60 = 一分鐘
+    // 1000 毫秒 * 60 * 30 = 三十分鐘
+    maxAge: 1000 * 60 * 30
+  },
+  // 是否保存未修改的 session
+  saveUninitialized: false,
+  // 是否每次重設過期時間
+  rolling: true,
+  resave: true
+}))
+
+let storage
+if (process.env.FTP === 'false') {
+  // 開發環境將上傳檔案放本機
+  storage = multer.diskStorage({
+    destination (req, file, cb) {
+      cb(null, 'images/')
+    },
+    filename (req, file, cb) {
+      cb(null, Date.now() + path.extname(file.originalname))
+    }
+  })
+} else {
+  // heroku 將上傳檔案放伺服器
+  storage = new FTPStorage({
+    // 上傳伺服器根目錄
+    basepath: '/',
+    // FTP設定
+    ftp: {
+      host: process.env.FTP_HOST,
+      secure: false,
+      user: process.env.FTP_USER,
+      password: process.env.FTP_PASSWORD
+    },
+    destination (req, file, options, cb) {
+      cb(null, options.basepath + Date.now() + path.extname(file.originalname))
+    }
+  })
+}
+
+const upload = multer({
+  storage,
+  fileFilter (req, file, cb) {
+    // 觸發 multer 錯誤，不接受檔案
+    // LIMIT_FORMAT 是自訂的錯誤 CODE，跟內建的錯誤 CODE 格式統一
+    if (!file.mimetype.includes('image')) {
+      cb(new multer.MulterError('LIMIT_FORMAT'), false)
+    } else {
+      cb(null, true)
+    }
+  },
+  limits: {
+    fileSize: 1024 * 1024
+  }
+})
+
+app.listen(process.env.PORT, () => {
+  console.log('已啟動')
+})
+
+// 註冊
+app.post('/register', async (req, res) => {
+  if (!req.headers['content-type'].includes('application/json')) {
+    res.status(400)
+    res.send({ success: false, message: '格式不符' })
+    return
+  }
+  try {
+    const result = await db.users.create({
+      account: req.body.account,
+      password: md5(req.body.password)
+    })
+    req.session.user = req.body.account
+    res.status(200)
+    res.send({ success: true, message: '', result })
+  } catch (error) {
+    res.status(400)
+    if (error.name === 'ValidationError') {
+      const key = Object.keys(error.errors)[0]
+      const message = error.errors[key].message
+      res.status(400)
+      res.send({ success: false, message })
+    } else {
+      res.status(500)
+      res.send({ success: false, message: '伺服器錯誤' })
+    }
+  }
+})
+
+// 登入
+app.post('/login', async (req, res) => {
+  if (!req.headers['content-type'].includes('application/json')) {
+    res.status(400)
+    res.send({ success: false, message: '格式不符' })
+    return
+  }
+  try {
+    const result = await db.users.find(
+      {
+        account: req.body.account,
+        password: md5(req.body.password)
+      }
+    )
+    if (result.length > 0) {
+      req.session.user = result[0].account
+      res.status(200)
+      res.send({ success: true, message: '', result })
+    } else {
+      res.status(404)
+      res.send({ success: false, message: '帳號密碼錯誤' })
+    }
+  } catch (error) {
+    res.status(400)
+    if (error.name === 'ValidationError') {
+      // 資料格式錯誤
+      const key = Object.keys(error.errors)[0]
+      const message = error.errors[key].message
+      res.status(400)
+      res.send({ success: false, message })
+    } else {
+      // 伺服器錯誤
+      res.status(500)
+      res.send({ success: false, message: '伺服器錯誤' })
+    }
+  }
+})
+
+// 登出
+app.delete('/logout', async (req, res) => {
+  req.session.destroy(error => {
+    if (error) {
+      res.status(500)
+      res.send({ success: false, message: '伺服器錯誤' })
+    } else {
+      res.clearCookie()
+      res.status(200)
+      res.send({ success: true, message: '' })
+    }
+  })
+})
+
+app.get('/heartbeat', async (req, res) => {
+  let islogin = false
+  if (req.session.user !== undefined) {
+    islogin = true
+  }
+  res.status(200)
+  res.send(islogin)
+})
+
+// 填寫會員資料
+app.patch('/memberprofile/:id', async (req, res) => {
+  if (!req.headers['content-type'].includes('application/json')) {
+    res.status(400)
+    res.send({ success: false, message: '格式不符' })
+    return
+  }
+  try {
+    const result = await db.users.findByIdAndUpdate(req.params.id, req.body, { new: true })
+    delete result.password
+    res.status(200)
+    res.send({ success: true, message: '', result })
+  } catch (error) {
+    if (error.name === 'CastError') {
+      res.status(404)
+      res.send({ success: false, message: '找不到資料' })
+    } else {
+      res.status(500)
+      res.send({ success: false, message: '伺服器發生錯誤' })
+    }
+    // console.log(error)
+  }
+})
+
+// 新增商品類別
+app.post('/sort', async (req, res) => {
+  // 沒有登入
+  // if (req.session.user === undefined) {
+  //   res.status(401)
+  //   res.send({ success: false, message: '未登入' })
+  //   return
+  // }
+  if (!req.headers['content-type'].includes('multipart/form-data')) {
+    res.status(400)
+    res.send({ success: false, message: '格式不符' })
+  }
+  // 有一個上傳進來的檔案，欄位是image
+  // req，進來的東西
+  // res，要出去的東西
+  // err，檔案上傳的錯誤
+  // upload.single(欄位)(req,res,上傳完畢的 function)
+  upload.single('image')(req, res, async error => {
+    if (error instanceof multer.MulterError) {
+      // 上傳錯誤
+      let message = ''
+      if (error.code === 'LIMIT_FILE_SIZE') {
+        message = '檔案太大'
+      } else {
+        message = '格式不符'
+      }
+      res.status(400)
+      res.send({ success: false, message })
+    } else if (error) {
+      res.status(500)
+      res.send({ success: false, message: '伺服器錯誤' })
+    } else {
+      try {
+        let sortsrc = ''
+        if (process.env.FTP === 'true') {
+          sortsrc = path.basename(req.file.path)
+        } else {
+          sortsrc = req.file.filename
+        }
+        const result = await db.sorts.create(
+          {
+            sortTitle: req.body.sortTitle,
+            sortName: req.body.sortName,
+            sortsrc
+          }
+        )
+        console.log(result)
+        res.status(200)
+        res.send({ success: true, message: '', result })
+      } catch (error) {
+        if (error.name === 'ValidationError') {
+          // 資料格式錯誤
+          const key = Object.keys(error.errors)[0]
+          const message = error.errors[key].message
+          res.status(400)
+          res.send({ success: false, message })
+        } else {
+          // 伺服器錯誤
+          res.status(500)
+          res.send({ success: false, message: '伺服器錯誤' })
+        }
+      }
+    }
+  })
+})
+
+// 取得商品類別路徑
+app.get('/sort/:name', async (req, res) => {
+  if (process.env.FTP === 'false') {
+    console.log('1' + req.params.name)
+    const path = process.cwd() + '/images/' + req.params.name
+    const exists = fs.existsSync(path)
+    if (exists) {
+      res.status(200)
+      res.sendFile(path)
+    } else {
+      res.status(404)
+      res.send({ success: false, message: '找不到圖片' })
+    }
+  } else {
+    res.redirect('http://' + process.env.FTP_HOST + '/' + process.env.FTP_USER + '/' + req.params.name)
+  }
+})
+
+// 取得商品類別資料
+app.get('/adminsort', async (req, res) => {
+  // if (req.session.user === undefined) {
+  //   res.status(401)
+  //   res.send({ success: false, message: '未登入' })
+  //   return
+  // }
+  // if (req.session.user !== req.params.user) {
+  //   res.status(403)
+  //   res.send({ success: false, message: '無權限' })
+  //   return
+  // }
+
+  try {
+    const result = await db.sorts.find()
+    res.status(200)
+    res.send({ success: true, message: '', result })
+  } catch (error) {
+    res.status(500)
+    res.send({ success: false, message: '伺服器錯誤' })
+  }
+})
+
+// 修改商品類別
+app.patch('/sort/:id', async (req, res) => {
+  // if (!req.headers['content-type'].includes('application/json')) {
+  //   res.status(400)
+  //   res.send({ success: false, message: '格式不符' })
+  //   return
+  // }
+  // 沒有登入
+  // if (!req.session.user) {
+  //   res.status(401)
+  //   res.send({ success: false, message: '無權限' })
+  //   return
+  // }
+
+  try {
+    // 檢查相片擁有者是不是本人
+    // let result = await db.files.findById(req.params.id)
+    // if (result.user !== req.session.user) {
+    //   res.status(403)
+    //   res.send({ success: false, message: '無權限' })
+    //   return
+    // }
+    // findByIdAndUpdate 預設回傳的是更新前的資料
+    // 設定 new true 後會變成回傳新的資料
+    const result = await db.sorts.findByIdAndUpdate(req.params.id, req.body, { new: true })
+    res.status(200)
+    res.send({ success: true, message: '', result })
+  } catch (error) {
+    if (error.name === 'CastError') {
+      // ID 格式不是 MongoDB 的格式
+      res.status(400)
+      res.send({ success: false, message: 'ID 格式錯誤' })
+    } else if (error.name === 'ValidationError') {
+      // 資料格式錯誤
+      const key = Object.keys(error.errors)[0]
+      const message = error.errors[key].message
+      res.status(400)
+      res.send({ success: false, message })
+    } else {
+      // 伺服器錯誤
+      res.status(500)
+      res.send({ success: false, message: '伺服器錯誤' })
+    }
+  }
+})
+
+// 刪除商品類別
+app.delete('/sort/:id', async (req, res) => {
+  // 沒有登入
+  // if (!req.session.user) {
+  //   res.status(401)
+  //   res.send({ success: false, message: '無權限' })
+  //   return
+  // }
+
+  try {
+    // 檢查相片擁有者是不是本人
+    // let result = await db.files.findById(req.params.id)
+    // if (result.user !== req.session.user) {
+    //   res.status(403)
+    //   res.send({ success: false, message: '無權限' })
+    //   return
+    // }
+    // findByIdAndDelete 預設回傳的是更新前的資料
+    // 設定 new true 後會變成回傳新的資料
+    const result = await db.sorts.findByIdAndDelete(req.params.id)
+    if (result === null) {
+      res.status(404)
+      res.send({ success: false, message: '找不到資料' })
+    } else {
+      res.status(200)
+      res.send({ success: true, message: '', result })
+    }
+  } catch (error) {
+    if (error.name === 'CastError') {
+      // ID 格式不是 MongoDB 的格式
+      res.status(400)
+      res.send({ success: false, message: 'ID 格式錯誤' })
+    } else if (error.name === 'ValidationError') {
+      // 資料格式錯誤
+      const key = Object.keys(error.errors)[0]
+      const message = error.errors[key].message
+      res.status(400)
+      res.send({ success: false, message })
+    } else {
+      // 伺服器錯誤
+      res.status(500)
+      res.send({ success: false, message: '伺服器錯誤' })
+    }
+  }
+})
+
+// 新增商品
+app.post('/product', async (req, res) => {
+  // 沒有登入
+  // if (req.session.user === undefined) {
+  //   res.status(401)
+  //   res.send({ success: false, message: '未登入' })
+  //   return
+  // }
+  if (!req.headers['content-type'].includes('multipart/form-data')) {
+    res.status(400)
+    res.send({ success: false, message: '格式不符' })
+  }
+  // 有一個上傳進來的檔案，欄位是image
+  // req，進來的東西
+  // res，要出去的東西
+  // err，檔案上傳的錯誤
+  // upload.single(欄位)(req,res,上傳完畢的 function)
+  upload.single('image')(req, res, async error => {
+    if (error instanceof multer.MulterError) {
+      // 上傳錯誤
+      let message = ''
+      if (error.code === 'LIMIT_FILE_SIZE') {
+        message = '檔案太大'
+      } else {
+        message = '格式不符'
+      }
+      res.status(400)
+      res.send({ success: false, message })
+    } else if (error) {
+      res.status(500)
+      res.send({ success: false, message: '伺服器錯誤' })
+    } else {
+      try {
+        let productsrc = ''
+        if (process.env.FTP === 'true') {
+          productsrc = path.basename(req.file.path)
+        } else {
+          productsrc = req.file.filename
+        }
+        const result = await db.products.create(
+          {
+            sortName: req.body.sortName,
+            productName: req.body.productName,
+            productPrice: req.body.productPrice,
+            productDescription: req.body.productDescription,
+            productStock: req.body.productStock,
+            productsrc
+          }
+        )
+        res.status(200)
+        res.send({ success: true, message: '', result })
+      } catch (error) {
+        if (error.name === 'ValidationError') {
+          // 資料格式錯誤
+          const key = Object.keys(error.errors)[0]
+          const message = error.errors[key].message
+          res.status(400)
+          res.send({ success: false, message })
+        } else {
+          // 伺服器錯誤
+          res.status(500)
+          res.send({ success: false, message: '伺服器錯誤' })
+        }
+      }
+    }
+  })
+})
+
+// 取得商品類別路徑
+app.get('/product/:name', async (req, res) => {
+  if (process.env.FTP === 'false') {
+    console.log('2' + req.params.name)
+    const path = process.cwd() + '/images/' + req.params.name
+    const exists = fs.existsSync(path)
+    if (exists) {
+      res.status(200)
+      res.sendFile(path)
+    } else {
+      res.status(404)
+      res.send({ success: false, message: '找不到圖片' })
+    }
+  } else {
+    res.redirect('http://' + process.env.FTP_HOST + '/' + process.env.FTP_USER + '/' + req.params.name)
+  }
+})
+
+// 取得商品資料
+app.get('/adminproduct', async (req, res) => {
+  // if (req.session.user === undefined) {
+  //   res.status(401)
+  //   res.send({ success: false, message: '未登入' })
+  //   return
+  // }
+  // if (req.session.user !== req.params.user) {
+  //   res.status(403)
+  //   res.send({ success: false, message: '無權限' })
+  //   return
+  // }
+
+  try {
+    const result = await db.products.find()
+    res.status(200)
+    res.send({ success: true, message: '', result })
+  } catch (error) {
+    res.status(500)
+    res.send({ success: false, message: '伺服器錯誤' })
+  }
+})
+
+// 修改商品
+app.patch('/product/:id', async (req, res) => {
+  if (!req.headers['content-type'].includes('application/json')) {
+    res.status(400)
+    res.send({ success: false, message: '格式不符' })
+    return
+  }
+  // 沒有登入
+  // if (!req.session.user) {
+  //   res.status(401)
+  //   res.send({ success: false, message: '無權限' })
+  //   return
+  // }
+
+  try {
+    // 檢查相片擁有者是不是本人
+    // let result = await db.files.findById(req.params.id)
+    // if (result.user !== req.session.user) {
+    //   res.status(403)
+    //   res.send({ success: false, message: '無權限' })
+    //   return
+    // }
+    // findByIdAndUpdate 預設回傳的是更新前的資料
+    // 設定 new true 後會變成回傳新的資料
+    const result = await db.products.findByIdAndUpdate(req.params.id, req.body, { new: true })
+    res.status(200)
+    res.send({ success: true, message: '', result })
+  } catch (error) {
+    if (error.name === 'CastError') {
+      // ID 格式不是 MongoDB 的格式
+      res.status(400)
+      res.send({ success: false, message: 'ID 格式錯誤' })
+    } else if (error.name === 'ValidationError') {
+      // 資料格式錯誤
+      const key = Object.keys(error.errors)[0]
+      const message = error.errors[key].message
+      res.status(400)
+      res.send({ success: false, message })
+    } else {
+      // 伺服器錯誤
+      res.status(500)
+      res.send({ success: false, message: '伺服器錯誤' })
+    }
+  }
+})
+
+// 刪除商品
+app.delete('/product/:id', async (req, res) => {
+  // 沒有登入
+  // if (!req.session.user) {
+  //   res.status(401)
+  //   res.send({ success: false, message: '無權限' })
+  //   return
+  // }
+
+  try {
+    // 檢查相片擁有者是不是本人
+    // let result = await db.files.findById(req.params.id)
+    // if (result.user !== req.session.user) {
+    //   res.status(403)
+    //   res.send({ success: false, message: '無權限' })
+    //   return
+    // }
+    // findByIdAndDelete 預設回傳的是更新前的資料
+    // 設定 new true 後會變成回傳新的資料
+    const result = await db.products.findByIdAndDelete(req.params.id)
+    if (result === null) {
+      res.status(404)
+      res.send({ success: false, message: '找不到資料' })
+    } else {
+      res.status(200)
+      res.send({ success: true, message: '', result })
+    }
+  } catch (error) {
+    if (error.name === 'CastError') {
+      // ID 格式不是 MongoDB 的格式
+      res.status(400)
+      res.send({ success: false, message: 'ID 格式錯誤' })
+    } else if (error.name === 'ValidationError') {
+      // 資料格式錯誤
+      const key = Object.keys(error.errors)[0]
+      const message = error.errors[key].message
+      res.status(400)
+      res.send({ success: false, message })
+    } else {
+      // 伺服器錯誤
+      res.status(500)
+      res.send({ success: false, message: '伺服器錯誤' })
+    }
+  }
+})
